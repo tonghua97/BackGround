ee478cb78b07d9b29298c9d910aeeec62ceb69a7
nabyss <1961542078@qq.com>
2016-11-22 09:22:47 +0800

    添加配置文件

--- foodfun_web/src/com/foodfun/common/config/FoodfunConfig.java
+++ foodfun_web/src/com/foodfun/common/config/FoodfunConfig.java
@@ -8,6 +8,9 @@ import com.jfinal.config.JFinalConfig;
 import com.jfinal.config.Plugins;
 import com.jfinal.config.Routes;
 import com.jfinal.core.JFinal;
+import com.jfinal.kit.PropKit;
+import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
+import com.jfinal.plugin.c3p0.C3p0Plugin;
 /**
  * API引导式配置
  */
@@ -16,7 +19,9 @@ public class FoodfunConfig extends JFinalConfig{
 	 * 配置常量
 	 */
 	public void configConstant(Constants me) {
-		me.setDevMode(true);  
+		// 加载少量必要配置，随后可用PropKit.get(...)获取值
+		PropKit.use("a_little_config.txt");
+		me.setDevMode(PropKit.getBoolean("devMode", false));
 	}
 	/**
 	 * 配置路由
@@ -24,10 +29,26 @@ public class FoodfunConfig extends JFinalConfig{
 	public void configRoute(Routes me) {
 		me.add("/hello", HelloController.class);  
 	}
+	
+	public static C3p0Plugin createC3p0Plugin() {
+		return new C3p0Plugin(PropKit.get("jdbcUrl"), PropKit.get("user"), PropKit.get("password").trim());
+	}
+	
 	/**
 	 * 配置插件
 	 */
-	public void configPlugin(Plugins me) {}
+	public void configPlugin(Plugins me) {
+		// 配置C3p0数据库连接池插件
+		C3p0Plugin C3p0Plugin = createC3p0Plugin();
+		me.add(C3p0Plugin);
+		
+		// 配置ActiveRecord插件
+		ActiveRecordPlugin arp = new ActiveRecordPlugin(C3p0Plugin);
+		me.add(arp);
+		
+		// 所有配置在 MappingKit 中搞定
+		_MappingKit.mapping(arp);
+	}
 	/**
 	 * 配置全局拦截器
 	 */
