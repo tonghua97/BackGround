74817a4ee0bef659f19c3dd3ab34959991eed2cb
nabyss <1961542078@qq.com>
2016-12-12 15:30:07 +0800

    完成界面编写

--- foodfun_web/src/com/foodfun/commend/CommendController.java
+++ foodfun_web/src/com/foodfun/commend/CommendController.java
@@ -10,13 +10,12 @@ import com.jfinal.core.Controller;
 import com.jfinal.upload.UploadFile;
 
 /**
- * BlogController
- * 锟斤拷锟斤拷 sql 锟斤拷业锟斤拷锟竭硷拷写锟斤拷 Model 锟斤拷 Service 锟叫ｏ拷锟斤拷要写锟斤拷 Controller 锟叫ｏ拷锟斤拷锟缴猴拷习锟竭ｏ拷锟斤拷锟斤拷锟节达拷锟斤拷锟斤拷目锟侥匡拷锟斤拷锟斤拷维锟斤拷
+ * CommendController
  */
 //@Before(BlogInterceptor.class)
 public class CommendController extends Controller {
 	public void index() {
-		setAttr("testPage", Commend.dao.paginate(getParaToInt(0, 1), 20));
+		setAttr("CommendPage", Commend.dao.paginate(getParaToInt(0, 1), 20));
 		render("commend.html");
 	}
 
--- foodfun_web/src/com/foodfun/fun/FunController.java
+++ foodfun_web/src/com/foodfun/fun/FunController.java
@@ -1,5 +1,7 @@
 package com.foodfun.fun;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import com.foodfun.common.model.Classify;
@@ -7,18 +9,20 @@ import com.foodfun.common.model.Fun;
 import com.foodfun.common.model.Mclassify;
 import com.foodfun.common.model.Recipes;
 import com.foodfun.common.model.Test;
+import com.foodfun.utils.CutImage;
 import com.jfinal.aop.Before;
 import com.jfinal.core.Controller;
+import com.jfinal.kit.PathKit;
+import com.jfinal.render.JsonRender;
 import com.jfinal.upload.UploadFile;
 
 /**
- * BlogController
- * 锟斤拷锟斤拷 sql 锟斤拷业锟斤拷锟竭硷拷写锟斤拷 Model 锟斤拷 Service 锟叫ｏ拷锟斤拷要写锟斤拷 Controller 锟叫ｏ拷锟斤拷锟缴猴拷习锟竭ｏ拷锟斤拷锟斤拷锟节达拷锟斤拷锟斤拷目锟侥匡拷锟斤拷锟斤拷维锟斤拷
+ * FunController
  */
 //@Before(BlogInterceptor.class)
 public class FunController extends Controller {
 	public void index() {
-		setAttr("testPage", Fun.dao.paginate(getParaToInt(0, 1), 20));
+		setAttr("FunPage", Fun.dao.paginate(getParaToInt(0, 1), 20));
 		render("fun.html");
 	}
 
@@ -26,22 +30,51 @@ public class FunController extends Controller {
 		
 	}
 	
-	
-	public void upload() {
+	public void upload(){
 		// 获取上传的文件
-		UploadFile uf = getFile("Filedata");
-
+		UploadFile uf = getFile("img_poster");
+		//获取文件名
+		String fileName = uf.getFileName();
+		
 		// 拼接文件上传的完整路径
-		String fileName = "http://" + this.getRequest().getRemoteHost() + ":"
+		String fileUrl = "http://" + this.getRequest().getRemoteAddr() + ":"
 				+ this.getRequest().getLocalPort() + "/upload/"
 				+ uf.getFileName();
-		
-		this.setAttr("fileName", fileName);
-		System.out.println("================fileName:"+fileName);
-		
+		//返回图片名称、url路径
+		setAttr("fileName", fileName);
+		setAttr("fileUrl", fileUrl);
+			
 		//以json格式进行渲染
-		renderJson();
+		render(new JsonRender().forIE());
+		//renderJson();
 	}
+	
+	public void ajaxImageCut() throws IOException{
+        String imageName =getPara("imgName");
+        int left= getParaToInt("x1");
+        int top= getParaToInt("y1");
+        int width= getParaToInt("cw");
+        int height= getParaToInt("ch");
+        System.out.println("================"+left);
+     
+        File source = new File(PathKit.getWebRootPath() + "\\upload\\" + imageName);
+        String fileName = source.getName();
+        //String prefix=fileName.substring(fileName.lastIndexOf(".")+1);
+        System.out.println(fileName);
+        
+        boolean isSave = CutImage.saveImage(source,PathKit.getWebRootPath()+"\\upload\\img\\" + fileName,top,left,width,height);
+        if(isSave){
+        	String url = "http://" + this.getRequest().getRemoteAddr() + ":"
+    				+ this.getRequest().getLocalPort() + "/upload/img/"
+    				+ fileName;
+            setAttr("msg", "图片更新成功！");
+            setAttr("url", url);
+        }else{
+        	setAttr("msg", "图片更新失败！");
+        }
+        renderJson();
+	}
+	
 	
 	public void save() {
 		getModel(Fun.class).save();
